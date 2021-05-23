8242109de15c4c4515fe6fea0da9f8d451ba1b90
Hyunsoo, Chung <admin@hyonsu.com>
2021-05-23 14:12:01 +0900

    Fix adofai parser

--- utils.js
+++ utils.js
@@ -3,12 +3,16 @@ String.prototype.replaceAll = function(org, dest) {
 }
 
 module.exports.ADOFAIParser = level => {
-    return JSON.parse(String(level).trim()
-        .replaceAll(', ,', ',')
-        .replaceAll('}\n', '},\n')
-        .replaceAll('},\n\t]', '}\n\t]')
-        .replaceAll(', },', ' },')
-        .replaceAll(', }', ' }')
-        .replaceAll('\n', '')
-        .replaceAll('}\n', '},\n'));
+    try {
+        return JSON.parse(level);
+    } catch(e) {
+        return JSON.parse(String(level).trim()
+            .replaceAll(', ,', ',')
+            .replaceAll('}\n', '},\n')
+            .replaceAll('},\n\t]', '}\n\t]')
+            .replaceAll(', },', ' },')
+            .replaceAll(', }', ' }')
+            .replaceAll('\n', '')
+            .replaceAll('}\n', '},\n'));
+    }
 }
